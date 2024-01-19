aa41439ef972e02b21eb179ce26bca2e8695cca6
boyty01 <boyty01@gmail.com>
2024-01-19 10:11:09 +0000

    http redirect

--- dmtserver/middleware/https_redirect.js
+++ dmtserver/middleware/https_redirect.js
@@ -2,15 +2,10 @@ const router = require("express").Router();
 
 router.use((req, res, next) => {
     // redirect if not https
-
-    if(process.env.NODE_ENV == "development") return next();
-
-    if(!req.secure) {
-        console.log("no secure route redirecting.");
+    if(!req.secure && process.env.NODE_ENV !== "development") {
         return res.redirect(`https://${req.headers.host}${req.url}`);        
     }
-
-    // is https
+    
     return next();
 })
 
